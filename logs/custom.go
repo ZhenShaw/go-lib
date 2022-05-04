a69e3b48df957d62b0694b14dabd1cc3e1212c9d
ZhenShaw <zhenshaw@qq.com>
2022-05-04 23:30:56 +0800

    add custom writer block option

--- logs/custom.go
+++ logs/custom.go
@@ -1,6 +1,7 @@
 package logs
 
 import (
+	"fmt"
 	"io"
 	"time"
 
@@ -9,11 +10,18 @@ import (
 )
 
 type CustomWriter struct {
-	logChan  chan string
+	// 投递日志数据的管道，容量是batch的数倍
+	logChan chan string
+	// 定时写日志间隔
 	interval time.Duration
-	batch    uint
-	cache    []string
-	handle   func([]string)
+	// 单次批量写日志的数量
+	batch uint
+	// 批量写日志缓存
+	cache []string
+	// 自定义的日志批量消费函数
+	handle func([]string)
+	// 是否阻塞写操作，默认false，当日志消费速度小于生产速度时，设置为true有可能引起日志消费不及时导致业务卡顿
+	block bool
 }
 
 func NewCustomWriter(handle func([]string), batch uint, interval time.Duration) *CustomWriter {
@@ -25,6 +33,10 @@ func NewCustomWriter(handle func([]string), batch uint, interval time.Duration) 
 	}
 	go w.Consume()
 	return w
+}
+
+func (w *CustomWriter) SetBlock(block bool) {
+	w.block = block
 }
 
 func (w *CustomWriter) Consume() {
@@ -52,6 +64,10 @@ func (w *CustomWriter) doConsume() {
 }
 
 func (w *CustomWriter) Write(b []byte) (int, error) {
+	if !w.block && len(w.logChan) == cap(w.logChan) {
+		err := fmt.Errorf("buffer overwrite, discard log: %s", string(b))
+		return 0, err
+	}
 	w.logChan <- string(b)
 	return 0, nil
 }
